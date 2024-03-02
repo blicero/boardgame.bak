05929fff4fef7a600fc0e596447ba74edeeea523
krylon <krylon>
2024-03-02 14:49:43 +0000

    Move piece to board.py

--- game/board.py
+++ game/board.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Time-stamp: <2024-03-01 11:34:17 krylon>
+# Time-stamp: <2024-03-02 15:48:20 krylon>
 #
 # /home/krylon/OneDrive/Dokumente/code/boardgame/server/board.py
 # created on 29. 02. 2024
@@ -33,7 +33,7 @@ board
 (c) 2024 Benjamin Walkenhorst
 """
 
-
+from dataclasses import dataclass
 from typing import NamedTuple
 
 
@@ -61,6 +61,39 @@ class Board:  # pylint: disable-msg=R0903
 
         for i in range(1, len(fields)):
             assert len(fields[i]) == len(fields[0])
+
+
+@dataclass(slots=True)
+class Position:
+    x: int
+    y: int
+
+
+@dataclass(slots=True, kw_only=True)
+class Piece:
+    """A game piece"""
+
+    pid: int
+    name: str
+    hp: int
+    ap: int
+    attack_range: int
+    pos: Position
+
+    def __init__(self, **fields):
+        self.pid = fields.get("pid", 0)
+        self.name = fields["name"]
+        self.hp = fields["hp"]
+        self.ap = fields["ap"]
+        self.attack_range = fields.get("attack_range", 1)
+        self.pos = fields.get("pos", Position(0, 0))
+
+        assert self.hp > 0, \
+            f"Invalid HP: {self.hp} (must be a positive integer)"
+        assert self.ap > 0, \
+            f"Invalid AP: {self.ap} (must be a positive integer)"
+        assert self.attack_range > 0, \
+            f"Invalid attack_range: {self.attack_range} (must be a positive integer)"  # noqa: E501
 
 
 # Local Variables: #
